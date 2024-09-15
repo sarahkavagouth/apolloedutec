63152d3dddaa6602ea61144db8cd56ce022ddbc2
sarahkavagouth <sarahkavagouth@outlook.com>
2024-09-15 10:36:55 -0300

    apollo sem js

--- paginas/conteudo.js
+++ paginas/conteudo.js
@@ -22,3 +22,23 @@ function prevSlide() {
 }
 
 showSlide(slideIndex);
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector('.header');
+  
+    function checkHeaderColor() {
+      const scrollPosition = window.scrollY || window.pageYOffset;
+  
+      if (scrollPosition >= 100) {
+        header.classList.add('black-bg');
+      } else {
+        header.classList.remove('black-bg');
+      }
+    }
+  
+    window.addEventListener('scroll', checkHeaderColor);
+    checkHeaderColor(); 
+  });
+  
